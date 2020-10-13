0d0baf8998fddc39b84506d8f330336188dde9ff
RomanPolulekh <70841623+RomanPolulekh@users.noreply.github.com>
2020-10-13 21:06:22 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let str=String(n);
+  let a=0; 
+  let boom='';
+  if(str[0]==='-'){
+boom=str[3]+str[2]+str[1];
+  } else
+ boom=str[2]+str[1]+str[0];
+    
+a=Number(boom);
+  return a;
 }
